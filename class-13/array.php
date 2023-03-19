6cb55852d2f0c7e0e8b1a0a99034ccfe871f225d
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-03-20 05:07:00 +0600

    fast modify
    r

---
+++ class-13/array.php
@@ -0,0 +1,4 @@
+<?php
+$array = [64,45,46,47,49,50];
+$size = sizeof($array);
+echo $size;
---
+++ class-3/area-of-triangle.php
@@ -0,0 +1,17 @@
+<?php
+$a = 6 ;
+$b = 7 ;
+$c = 8 ;
+
+$s = ($a + $b = $c)/2 ;
+if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b);
+{
+    $area = sqrt ($s*($s-$a) ($s-$b) ($s-$c));
+    echo "the area of this tringle".round($area);
+}
+else
+{
+    echo "this Tringle is imaginary";
+}
+
+?>
