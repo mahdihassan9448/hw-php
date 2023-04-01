50ce00aed38237a761db63ebe289616a318106cb
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-01 19:54:26 +0600

    hfljohfu

---
+++ class-21/passcal.php
@@ -0,0 +1,13 @@
+<?php
+
+for($i = 0; $i <= 6; $i++){  
+        for($k = 5; $k >= $i; $k--){  
+            echo "&nbsp;&nbsp";  
+        }  
+        for($j = 1;$j <= $i; $j++){  
+            echo "&nbsp;# ";  
+        }  
+        echo "<br>";  
+        }	  
+
+ 
