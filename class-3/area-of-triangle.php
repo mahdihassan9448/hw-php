8ea01ebb36503946c03f7b3f1279b81ec5174942
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-05 19:36:37 +0600

    hsgufgwbfghyifgh juhf h

--- class-3/area-of-triangle.php
+++ class-3/area-of-triangle.php
@@ -1,4 +1,21 @@
 <?php
+// $a = 6 ;
+// $b = 7 ;
+// $c = 8 ;
+
+// $s = ($a + $b = $c)/2 ;
+// if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b);
+// {
+//     $area = sqrt ($s*($s-$a) ($s-$b) ($s-$c));
+//     echo "the area of this tringle".round($area);
+// }
+// else
+// {
+//     echo "this Tringle is imaginary";
+// }
+
+
+
 $a = 6 ;
 $b = 7 ;
 $c = 8 ;
--- class-5/triangl-earea.php
+++ class-5/triangl-earea.php
@@ -1,14 +1,27 @@
 <?php
 
-    $height = 10;
-    $base = 12;
-    $area = 0.5 * $height * $base;
-    echo "Area of the triangle : ".$area;
+//     $height = 10;
+//     $base = 12;
+//     $area = 0.5 * $height * $base;
+//     echo "Area of the triangle : ".$area;
 
-    echo "<br>";
-    echo "<br>";
-    echo "<br>";
+//     echo "<br>";
+//     echo "<br>";
+//     echo "<br>";
 
-   $b = (2 * $area ) / $height;
+//    $b = (2 * $area ) / $height;
 
-   echo $b;
+//    echo $b;
+
+$height = 10;
+$base = 12;
+$area = 0.5 * $height * $base;
+echo "Area of the triangle : ".$area;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$b = (2 * $area ) / $height;
+
+echo $b;
