67c8d6210f94736819e41d4f747a3012b865ea20
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-06 20:47:49 +0600

    new coment

--- class-1/biggest.php
+++ class-1/biggest.php
@@ -1,4 +1,23 @@
 <?php
+// $m1=87;
+// $m2=54;
+// $m3=98;
+
+// if ($m1 > $m2 && $m1 > $m3)
+//     {
+//         echo "biggest number:".$m1;
+//     }
+//     else if($m2 > $m1 && $m2 > $m3)
+//     {
+//         echo "biggest number:".$m2;
+//     }
+//     else
+//     {
+//         echo "biggest number:".$m3;
+//     }
+
+
+
 $m1=87;
 $m2=54;
 $m3=98;
