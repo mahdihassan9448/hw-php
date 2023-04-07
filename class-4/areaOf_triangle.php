88b53b2bc52e0c485a66ab8519280708efebf24f
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-07 17:23:59 +0600

    modifid

---
+++ class-4/areaOf_triangle.php
@@ -0,0 +1,8 @@
+<?php
+    $area = 45;
+    $hight = 20;
+    $base = (2*$area)/$hight;
+
+    // echo $base;
+
+    echo (int) $base;
---
+++ class-4/celcius_to_farenheit.php
@@ -0,0 +1,8 @@
+<?php
+    $f = 98;
+    $celcius = (5*($f-32))/9;
+    echo $celcius. "<br>". "<br>";
+
+    $celcius = 50;
+    $f = ((9*$celcius)/5)+32;
+    echo $f;
