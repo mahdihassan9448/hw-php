175a6a51df881707ba666eeea0e7ebd9f387c276
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-07 11:46:36 +0600

    new

--- class-3/area-of-triangle.php
+++ class-3/area-of-triangle.php
@@ -3,10 +3,10 @@
 // $b = 7 ;
 // $c = 8 ;
 
-// $s = ($a + $b = $c)/2 ;
-// if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b);
+// $s = ($a + $b + $c)/2 ;
+// if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b)
 // {
-//     $area = sqrt ($s*($s-$a) ($s-$b) ($s-$c));
+//     $area = sqrt ($s*($s-$a)*($s-$b)*($s-$c));
 //     echo "the area of this tringle".round($area);
 // }
 // else
@@ -16,15 +16,15 @@
 
 
 
-$a = 6 ;
-$b = 7 ;
-$c = 8 ;
+$a = 10 ;
+$b = 21 ;
+$c = 30 ;
 
-$s = ($a + $b = $c)/2 ;
-if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b);
+$s = ($a + $b + $c)/2 ;
+if ( ($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b)
 {
-    $area = sqrt ($s*($s-$a) ($s-$b) ($s-$c));
-    echo "the area of this tringle".round($area);
+    $area = sqrt ($s*($s-$a)*($s-$b)*($s-$c));
+    echo "the area of this tringle".($area);
 }
 else
 {
---
+++ class-4/circle_area.php
@@ -0,0 +1,11 @@
+<?php
+    define('PI',3.1416);
+
+    $r = 5;
+    $area = PI*$r*$r;
+
+    $circunference = 2*PI*$r;
+
+    echo $area."<br>"."<br>";
+
+    echo $circunference;
