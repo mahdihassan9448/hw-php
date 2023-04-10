b089e31f7c0a8d46e4a9518006b8200889ae06f6
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-10 23:48:28 +0600

    fjjif

---
+++ class-6/odd_even.php
@@ -0,0 +1,11 @@
+<?php
+    $number = 448755;
+    $reminder = $number % 2;
+    if($reminder == 0)
+    {
+        echo "number is even";
+    }
+    else 
+    {
+        echo "number is odd";
+    }
