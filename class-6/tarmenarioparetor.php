ff9466a685d60c23932f3e26f6b31d69844151e4
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-04-11 23:14:29 +0600

    jhty

--- class-6/tarmenarioparetor.php
+++ class-6/tarmenarioparetor.php
@@ -2,14 +2,14 @@
     $a1 = 83;
     $a2 = 8374;
 
-    echo $a1>$a2 ? $a1:$a2;
+    // echo $a1>$a2 ? $a1:$a2;
 
-    // if($a1 > $a2)
-    // {
-    //     echo $a1;
-    // }
-    // else
-    // {
-    //     echo $a2;
-    // }
+    if($a1 > $a2)
+    {
+        echo $a1;
+    }
+    else
+    {
+        echo $a2;
+    }
 
