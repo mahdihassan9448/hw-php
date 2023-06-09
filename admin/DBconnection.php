28f7be7d5312dd6629dd989107516973eb71de54
mahdihassan9448 <mahdihassan9448@gmail.com>
2023-06-09 23:41:46 +0600

    admin

---
+++ admin/DBconnection.php
@@ -0,0 +1,14 @@
+<?php
+
+$Host  = 'localhost';
+$Name  = 'root';
+$Pass  = '';
+$DB    = 'dashbord';
+
+$Connection = mysqli_connect($Host, $Name, $Pass, $DB);
+
+if(!$Connection){
+    echo "DB connection failed";
+}
+
+?>
